69140e8ccb9bd8760a48d64cc063a375bb30e526
Brigs <abrignoni@gmail.com>
2018-08-21 10:58:31 -0400

    Update iOS-Nike-geo2csv.py
    Added some comments.

--- iOS-Nike-geo2csv.py
+++ iOS-Nike-geo2csv.py
@@ -16,14 +16,14 @@ rows = cursor.fetchall()
 for row in rows:
 	runid = (row[0])
 	#print(runid)
-	
+	#select the first row of every run to extract the date and time. Those will be used to name the csv.
 	cursor1.execute("SELECT lat.sourceid, datetime(lat.startDateInUtcSeconds, 'unixepoch', 'localtime') as day, lat.startDateInUtcSeconds, lat.value as latitude, long.value as long FROM metrics as lat left join metrics as long on lat.startDateInUtcSeconds = long.startDateInUtcSeconds WHERE lat.type = 'latitude' and long.type = 'longitude' and lat.sourceID = ? ORDER BY lat.startDateInUtcSeconds LIMIT 1", (runid,))
 	rows1 = cursor1.fetchall()
 	for row in rows1:
 		rundate = row[1]
 		#print(rundate)
 		titlefile = rundate.replace(":", "-")
-	
+	#write the csv
 	cursor.execute("SELECT lat.sourceid, datetime(lat.startDateInUtcSeconds, 'unixepoch', 'localtime') as day, lat.startDateInUtcSeconds, lat.value as latitude, long.value as long FROM metrics as lat left join metrics as long on lat.startDateInUtcSeconds = long.startDateInUtcSeconds WHERE lat.type = 'latitude' and long.type = 'longitude' and lat.sourceID = ? ORDER BY lat.startDateInUtcSeconds", (runid,))
 	rows = cursor.fetchall()
 	csvWriter = csv.writer(open(titlefile +'.csv', 'w', newline=''))
